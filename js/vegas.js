4f2f49b8f6df129d201a6a9f52460671b4843bee
yoooooshitooooo <mizuiro172215453@gmail.com>
2025-06-15 20:43:23 +0900

    Create vegas.js

---
+++ js/vegas.js
@@ -0,0 +1,14 @@
+$(function() {
+
+    $('#mainimg').vegas({
+        slides: [
+            { src: './images/1.jpg' },	//1枚目の写真指定
+            { src: './images/2.jpg' },	//2枚目の写真指定
+            { src: './images/3.jpg' },	//3枚目の写真指定
+        ],
+		overlay: true,				//写真上にドットを重ねる。不要ならこの１行を削除して下さい。
+		transition: 'fade',			//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
+		delay: 5000,				//次の画像を表示するまでの時間
+		animationDuration: 20000,	//アニメーション間の引き継ぎタイミング。
+    });
+});
